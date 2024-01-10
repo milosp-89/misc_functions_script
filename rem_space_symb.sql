95023ff0b25e67b8e6af7c4fa8cb147fde3b2a8d
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-01-10 13:30:28 +0100

    Create rem_space_symb.sql

---
+++ rem_space_symb.sql
@@ -0,0 +1,24 @@
+-- function to remove spaces and symbols from a text/string
+-- add more symbols if needed!
+
+use [xxx]
+go;
+
+create function [dbo].[rem_space_symb]
+(
+    @input_string nvarchar(max)
+)
+returns nvarchar(max)
+as
+begin
+    declare @output_string nvarchar(max)
+    set @output_string = 
+	replace(replace(replace(replace(replace(replace(replace(replace
+		(replace(replace(replace(replace(replace(replace(replace(replace
+			(@input_string, ' ', ''), '..', ','), ';;', ',,'), '::', ','),
+							'//', ','), '--', ','), '\\', ','), '##', ','),
+							'.', ','), ';', ','), ':', ','), '/', ','),
+							'-', ','), '\', ','), '#', ','), ',', ', ')
+    return @output_string
+end;
+go
