7c3ef4f1447e64e02c0a3b92983183902e75c02e
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-01-10 13:26:30 +0100

    Create geo_loc.sql

---
+++ geo_loc.sql
@@ -0,0 +1,26 @@
+-- function for extracting geo location (lang/long)
+
+use [xxx]
+go;
+
+create function [dbo].[geo_loc] (
+  @column nvarchar(max),
+	@position tinyint,
+	@value nvarchar(50))
+returns nvarchar(max)
+as
+begin
+	declare @output_string nvarchar(max)
+	set @output_string = 
+	replace(
+		reverse(
+		replace(
+		parsename(
+		replace(
+		replace(
+		reverse(@column),
+		'.', '_'), ',', '.'), @position), '_', '.')),  @value, '')
+
+return @output_string
+end
+go;
