dd6acf9b70f1309f56a63bcf4d749beb53c37a14
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-01-17 16:49:37 +0100

    Update submission_time.sql

--- submission_time.sql
+++ submission_time.sql
@@ -9,11 +9,11 @@ as
 begin
     declare @final decimal(10, 2);
     set @final = cast
-					(round
-						(datediff
-							(second, @start_question,
-									 left(@submitted_at_question, charindex('+', @submitted_at_question) - 1)) 
-									 / cast(60 AS decimal(10, 2)), 2) as decimal(10, 2));
+	    (round
+	    	(datediff
+	    		(second, @start_question,
+	    		left(@submitted_at_question, charindex('+', @submitted_at_question) - 1)) /
+	    		cast(60 AS decimal(10, 2)), 2) as decimal(10, 2));
     return @final;
 end;
 go
